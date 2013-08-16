1382379a18617f04ae80e51788bd93c264c7e376
sawa <mostlyfine@gmail.com>
2013-08-16 15:46:13 +0900

    knife solo prepare vagrant@192.168.33.10 -i ~/.ssh/id_rsa

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,7 +1,9 @@
-cookbook_path ["cookbooks", "site-cookbooks"]
-node_path     "nodes"
-role_path     "roles"
-data_bag_path "data_bags"
-#encrypted_data_bag_secret "data_bag_key"
-
-knife[:berkshelf_path] = "cookbooks"
+log_level                :info
+log_location             STDOUT
+node_name                'mostlyfine'
+client_key               '/Users/sawayanagi/.chef/mostlyfine.pem'
+validation_client_name   'chef-validator'
+validation_key           '/etc/chef/validation.pem'
+chef_server_url          'http://host078.in.1101.com:4000'
+syntax_check_cache_path  '/Users/sawayanagi/works/vagrant/chef-repo/sample/.chef/syntax_check_cache'
+cookbook_path            ['./cookbooks', './site-cookbooks']
