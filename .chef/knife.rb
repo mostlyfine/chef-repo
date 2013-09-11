8b312192675ac53017634046a76977cabab2d001
sawa <mostlyfine@gmail.com>
2013-09-11 15:53:56 +0900

    パス指定を相対パスベースに

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,11 +1,11 @@
 log_level                :info
 log_location             STDOUT
 node_name                'mostlyfine'
-client_key               '/Users/sawayanagi/.chef/mostlyfine.pem'
+client_key               '~/.chef/mostlyfine.pem'
 validation_client_name   'chef-validator'
 validation_key           '/etc/chef/validation.pem'
 chef_server_url          'http://host078.in.1101.com:4000'
-syntax_check_cache_path  '/Users/sawayanagi/works/vagrant/chef-repo/sample/.chef/syntax_check_cache'
+syntax_check_cache_path  '.chef/syntax_check_cache'
 cookbook_path            ['./cookbooks', './site-cookbooks']
 role_path                './roles'
 data_bag_path            './data_bags'
